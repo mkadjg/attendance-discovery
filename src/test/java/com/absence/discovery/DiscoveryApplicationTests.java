7902b2562f44c6607e88ce2a6a8214ff62db7a1b
Sandi Saputra <saputra.sndi@outlook.com>
2022-05-20 18:25:16 +0700

    initial commit

---
+++ src/test/java/com/absence/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absence.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
